de0e688fd3fd119da32275ad594f78906c4e7467
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-07-17 17:12:44 -0500

    Se añadió servicio de exchange rate

---
+++ SistemaBancario/exchange-rate-api/src/main/java/com/exchange_rate_api/exchange_rate_api/ExchangeRateApiApplication.java
@@ -0,0 +1,13 @@
+package com.exchange_rate_api.exchange_rate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateApiApplication.class, args);
+	}
+
+}
