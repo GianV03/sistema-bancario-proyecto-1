ef4774137252edfb2be5260ecf076b955fbc4018
gianfranco <gianfranco.vives.03@gmail.com>
2024-07-22 11:24:23 -0500

    Se añadió api gatewat

--- SistemaBancario/accounts-api/src/main/java/com/example/accounts_api/AccountsApiApplication.java
+++ SistemaBancario/accounts-api/src/main/java/com/example/accounts_api/AccountsApiApplication.java
@@ -2,8 +2,10 @@ package com.example.accounts_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountsApiApplication {
 
 	public static void main(String[] args) {
---
+++ SistemaBancario/discovery-server/src/test/java/com/sistema_bancario/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_bancario.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
