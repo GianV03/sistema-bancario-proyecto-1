e43df86c5b2142309a5f971347a655a2b2385937
gianfranco <gianfranco.vives.03@gmail.com>
2024-07-22 14:42:07 -0500

    Se añadieron todos los microservicios al gateway

--- SistemaBancario/yanki-api/src/main/java/com/yanki_api/yanki_api/YankiApiApplication.java
+++ SistemaBancario/yanki-api/src/main/java/com/yanki_api/yanki_api/YankiApiApplication.java
@@ -2,8 +2,10 @@ package com.yanki_api.yanki_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class YankiApiApplication {
 
 	public static void main(String[] args) {
--- SistemaBancario/yanki-api/src/main/java/com/yanki_api/yanki_api/controllers/PurseController.java
+++ SistemaBancario/yanki-api/src/main/java/com/yanki_api/yanki_api/controllers/PurseController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/purses")
+@RequestMapping("/api/purses")
 public class PurseController {
 
     @Autowired
